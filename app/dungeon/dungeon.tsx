7c2d0d2d159aa7d230d1191949474888d2cee03e
RaphaelTheStegosaurus <r.a.s.m.13@hotmail.com>
2025-09-04 13:59:46 -0700

    Third commit Player Sprite mAnager functional Player orientation position and resize Player send ref to parent container

--- app/dungeon/dungeon.tsx
+++ app/dungeon/dungeon.tsx
@@ -1,11 +1,18 @@
+import { useRef } from "react";
 import style from "./dungeon.module.css";
 import Player from "./player/player";
 import Rooms from "./rooms/rooms";
 export default function Dungeon() {
+  const PLAYER_REF = useRef(null);
   return (
     <div className={`${style.dungeon}`}>
-      <Rooms room="room1"/>
-      <Player/>
+      <Rooms room="room1" />
+      <Player
+        orientation="SOUTH"
+        attributes={{ x: 50, y: 20, width: 50, height: 50 }}
+        spriteSelector={0}
+        ref={PLAYER_REF}
+      />
     </div>
   );
 }
--- app/dungeon/player/player.tsx
+++ app/dungeon/player/player.tsx
@@ -1,22 +1,41 @@
+import { forwardRef } from "react";
 import style from "./player.module.css";
-interface Props {}
-
-export default function Player({}: Props) {
-  function SetOrientation() {}
-  function SetSize() {}
-  function SetPosition() {}
-  function SetSprite() {}
-  return (
-    <div
-      className={`${style.player}`}
-      style={{
-        width: "30px",
-        height: "30px",
-        top: "30px",
-        left: "30px",
-        backgroundImage: `url("/walk-b.png")`,
-        backgroundSize: "200% 100%",
-      }}
-    ></div>
-  );
+interface Props {
+  attributes: {
+    x: number;
+    y: number;
+    height: number;
+    width: number;
+  };
+  orientation: "NORTH" | "SOUTH" | "EAST" | "WEST";
+  spriteSelector: 0 | 1;
 }
+
+const Player = forwardRef(
+  ({ attributes, orientation, spriteSelector }: Props, ref) => {
+    const SpritesConfig = {
+      NORTH: "/walk-t.png",
+      SOUTH: "/walk-b.png",
+      EAST: "/walk-i.png",
+      WEST: "/walk-i.png",
+    };
+    const SpritePosition = ["0% 0%", "100% 0%"];
+    return (
+      <div
+        className={`${style.player}`}
+        style={{
+          width: `${attributes.width}px`,
+          height: `${attributes.height}px`,
+          top: `${attributes.y}px`,
+          left: `${attributes.x}px`,
+          backgroundImage: `url(${SpritesConfig[orientation]})`,
+          backgroundSize: "200% 100%",
+          backgroundPosition: SpritePosition[spriteSelector],
+          transform: `scaleX(${orientation === "WEST" ? -1 : 1})`,
+        }}
+      ></div>
+    );
+  }
+);
+
+export default Player;
