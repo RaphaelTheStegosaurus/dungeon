574edebbbfa9b74fc6c17bb14b9c635d0c412eed
RaphaelTheStegosaurus <r.a.s.m.13@hotmail.com>
2025-09-15 21:35:30 -0700

    Start Refactoring

---
+++ app/dungeon/lib/constants.ts
@@ -0,0 +1,36 @@
+import { rectAttribute, RoomDoorFaces } from "../types";
+
+export const PLAYER_VELOCITY = 20;
+export const WALLS_WIDTH = 20;
+export const DOOR_SAMPLE: rectAttribute = {
+  x: 0,
+  y: 0,
+  height: 20,
+  width: 75,
+};
+
+export const SETS_OF_FACES_BY_ROOM: RoomDoorFaces = {
+  room1: ["door-face-bottom", "door-hided-top", "door-hided-bottom"],
+  room2: ["door-face-top", "door-face-bottom", "door-hided-bottom"],
+  room3: ["door-hided-top", "door-face-top", "door-face-bottom"],
+  room4: ["door-hided-top", "door-hided-top", "door-face-top"],
+};
+
+export const ASIDE_ROOMS = {
+  room1: {
+    prev: undefined,
+    next: "room2",
+  },
+  room2: {
+    prev: "room1",
+    next: "room3",
+  },
+  room3: {
+    prev: "room2",
+    next: "room4",
+  },
+  room4: {
+    prev: "room3",
+    next: undefined,
+  },
+};
---
+++ app/dungeon/lib/util.ts
@@ -0,0 +1,76 @@
+import {
+  Coord,
+  DoorAttribute,
+  DoorFace,
+  rectAttribute,
+  room,
+  Sizes,
+} from "../types";
+import { WALLS_WIDTH, DOOR_SAMPLE,ASIDE_ROOMS } from "./constants"; 
+
+export const checkIfPlayerEnteringTheDoor = (
+  playerRect: rectAttribute,
+  doors: DoorAttribute[]
+): DoorAttribute | undefined => {
+  for (const door of doors) {
+    if (
+      playerRect.x < door.x + door.width &&
+      playerRect.x + playerRect.width > door.x &&
+      playerRect.y < door.y + door.height &&
+      playerRect.y + playerRect.height > door.y
+    ) {
+      return door;
+    }
+  }
+  return undefined;
+};
+export const checkBoundaries = (
+  newX: number,
+  newY: number,
+  playerAttributes: rectAttribute,
+  roomSize: Sizes
+): Coord => {
+  const minX = 0 + WALLS_WIDTH;
+  const minY = 0 + WALLS_WIDTH;
+  const maxX = roomSize.width - playerAttributes.width - WALLS_WIDTH;
+  const maxY = roomSize.height - playerAttributes.height - WALLS_WIDTH;
+  const constrainedX = Math.min(Math.max(newX, minX), maxX);
+  const constrainedY = Math.min(Math.max(newY, minY), maxY);
+  return { x: constrainedX, y: constrainedY };
+};
+
+export const returnDoorPositionY = (
+  _doorFace: DoorFace,
+  _roomSize: Sizes
+): number => {
+  switch (_doorFace) {
+    case "door-hided-top":
+      return -(DOOR_SAMPLE.height * 2);
+    case "door-face-top":
+      return WALLS_WIDTH;
+    case "door-face-bottom":
+      return _roomSize.height - DOOR_SAMPLE.height - WALLS_WIDTH;
+    case "door-hided-bottom":
+      return _roomSize.height + DOOR_SAMPLE.height * 2;
+    default:
+      return DOOR_SAMPLE.y;
+  }
+};
+
+export const changeRoom = (
+  _doorFace: DoorFace,
+  currentRoom: room,
+) => {
+  const currentAsideRoom = ASIDE_ROOMS[currentRoom];
+  if (_doorFace === "door-face-top") {
+    if (currentAsideRoom.prev) {
+      return currentAsideRoom.prev;
+    }
+  }
+  if (_doorFace === "door-face-bottom") {
+    if (currentAsideRoom.next) {
+      return currentAsideRoom.next;
+    }
+  }
+  return currentRoom;
+};
