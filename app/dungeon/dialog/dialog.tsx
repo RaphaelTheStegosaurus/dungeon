74887ae546b9b9a689395a8224568d01d36d1b76
RaphaelTheStegosaurus <r.a.s.m.13@hotmail.com>
2025-09-20 23:40:41 -0700

    Fixing the styles of the dpad.tsx

---
+++ app/dungeon/dialog/dialog.tsx
@@ -0,0 +1,16 @@
+interface Props {}
+
+const Dialog = (props: Props) => {
+  return (
+    <div>
+      <h2>-Personaje-</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum, quis
+        aliquam consequuntur qui maiores distinctio debitis deleniti dolores est
+        hic ipsam enim molestiae beatae, recusandae laudantium dolore alias amet
+        veritatis?
+      </p>
+    </div>
+  );
+};
+export default Dialog;
