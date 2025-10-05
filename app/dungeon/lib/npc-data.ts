56383526e6b454e899c14e8f093ee429d9881d55
RaphaelTheStegosaurus <r.a.s.m.13@hotmail.com>
2025-10-04 23:09:50 -0700

    Working with second dialog with second npc

--- app/dungeon/lib/npc-data.ts
+++ app/dungeon/lib/npc-data.ts
@@ -12,8 +12,17 @@ export const DIALOGS = [
     text: "Hola me llamo Rafael Alberto Serrato Morales",
   },
   {
-    name: "Rafael Alberto The Stegosaurus",
-    text: "Hola me llamo Rafael Alberto Serrato Morales",
+    name: "Tu Segundo NPC",
+    text: `
+    Hola mucho gusto de verme soy el segundo npc, 
+    ademas quería habar de que lo mas conveniente 
+    del uso de mas variables en entre el padre y el hijo 
+    sea que crees un flujo de variables seguro donde puedan 
+    interactuar de manera pacifica al resto de la function y
+     que este no afecte el flujo de carga ademas de aparte la 
+     mayoría de funciones esto te dejaría libre de sobre saturation 
+     del programa y estaría mas dividido checa las descripción
+     del hook usecontext `,
   },
   {
     name: "Rafael Alberto The Stegosaurus",
